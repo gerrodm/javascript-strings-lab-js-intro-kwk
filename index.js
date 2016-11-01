f16b2eeb44d93b3c1b5168f4e3b9dfcb5eff40a7
gerrod <gerrodmilton55@gmail.com>
2016-11-01 17:57:27 +0000

    new message

---
+++ index.js
@@ -0,0 +1,18 @@
+var greeting = "Hello, everybody!";
+
+var specialGuest = "Niel deGrasse Tyson"
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic = "";
+
+var conversation = `Let\'s talk about .${topic}`;
+
+var writeGreetingToDocument = `document.getElementById(\'greeting\').innerHTML = greeting;`;
+
+// Type your answer into the console without the quote marks and see if it works.
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = `console.log(customCelebration);`;
+
